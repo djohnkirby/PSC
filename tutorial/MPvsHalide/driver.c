bcb1d8aa3b90699b62fcfde666bf99eec57f0c6c
djohnkirby <dkirby@cmu.edu>
2014-02-18 15:34:47 -0500

    Started driver.c

---
+++ tutorial/MPvsHalide/driver.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "getline.h"
+#include <sys/time.h>
+#include <omp.h>
+
+int main()
+{
+	/* I will use this timer sev'ral times. */
+	struct timeval start_time, stop_time, elapsed_time;
+	
+}
