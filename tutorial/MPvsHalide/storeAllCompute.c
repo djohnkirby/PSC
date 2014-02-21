9c2589cd65c42dbf8cd442fd290c472c1c6f8457
djohnkirby <dkirby@cmu.edu>
2014-02-21 15:30:50 -0500

    segtown

--- tutorial/MPvsHalide/storeAllCompute.c
+++ tutorial/MPvsHalide/storeAllCompute.c
@@ -8,26 +8,23 @@
 float ** storeAllCompute()
 {
 	int x, y, i, j;
-	int numPasses = CONSUMER_HEIGHT/STRIPE_SIZE;
 	float producer_arr[PRODUCER_WIDTH][PRODUCER_HEIGHT];
 	float ** consumer_arr;
 	int correctness = 0;
 
-	if( CONSUMER_HEIGHT % STRIPE_SIZE )
-		numPasses ++; 
-
 	consumer_arr = malloc(CONSUMER_HEIGHT*sizeof(float*));
-        for( i = 0; i < CONSUMER_HEIGHT; i ++ )
+        for( i = 0; i < CONSUMER_WIDTH; i ++ )
                 consumer_arr[i] = malloc(CONSUMER_WIDTH*sizeof(float));
-
+	printf("CONSUMER_HEIGHT is %d, PRODUCER_HEIGHT IS %d, PRODUCER_WIDTH\
+				  is %d\n", i, PRODUCER_HEIGHT, PRODUCER_WIDTH);
 	for( x = 0; x < PRODUCER_WIDTH; x ++ )
 		for( y = 0; y < PRODUCER_HEIGHT; y ++ )
 			producer_arr[x][y] = producer(x, y);
 
+
 	for( x = 0; x < CONSUMER_WIDTH; x ++ )
 		for( y = 0; y < CONSUMER_HEIGHT; y ++ )
-			consumer_arr[x][y] = producer_arr[x][y] + producer_arr[x+1][y + 1]
-        + producer_arr[x+1][y] + producer_arr[x][y + 1];
+			consumer_arr[x][y] = producer_arr[x][y] + producer_arr[x+1][y + 1] + producer_arr[x+1][y] + producer_arr[x][y + 1];
 
 
 	return consumer_arr;
