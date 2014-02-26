7c5c3d759de4183c5eac8820f8c67500a9d72c16
Daniel John Kirby <dkirby@cmu.edu>
2014-02-26 11:47:00 -0500

    added scanline solution, have not tested yet.

---
+++ tutorial/MPvsHalide/noStoreComputeScanline.c
@@ -0,0 +1,31 @@
+/* This is a function that I am writing to compare to lesson_08 of the Halide
+	 tutorial.
+
+	Written by Daniel John Kirby.*/
+#include "MPvsHalide.h"
+
+float ** noStoreComputeByScanline()
+{
+	int x, y, i, j;
+	int yo, y_base, y_in, yi, py;
+	float ** consumer_arr;
+	float storeMe;
+	int numPasses = CONSUMER_HEIGHT/STRIPE_SIZE;
+	//int correctness = 0;
+
+	if( CONSUMER_HEIGHT % STRIPE_SIZE )
+		numPasses ++; 
+
+	/* Allocate conusmer_arr*/
+	consumer_arr = calloc(CONSUMER_WIDTH, sizeof(float*));
+	for( i = 0; i < CONSUMER_WIDTH; i ++ )
+		consumer_arr[i] = calloc(CONSUMER_HEIGHT, sizeof(float));	
+	/*This loop executes the producer and consumer function in parallel strips
+		of 16*/
+	#pragma omp parallel for private(x, y) shared(consumer_arr)
+	for( x = 0; x < CONSUMER_WIDTH; x ++ )
+		for( y = 0; y < CONSUMER_HEIGHT; y ++ )
+			consumer_arr[x][y] = consumer_raw(x,y);
+	/* Just ofr the luls, let's redo the first row*/
+	return consumer_arr;
+}
---
+++ tutorial/MPvsHalide/storeBuffCompute_ACC.c
@@ -0,0 +1,59 @@
+/* This is a function that I am writing to compare to lesson_08 of the Halide
+	 tutorial.
+
+	Written by Daniel John Kirby.*/
+
+#include "MPvsHalide.h"
+
+float ** storeBuffCompute()
+{
+	int x, y, i, j;
+	int yo, y_base, y_in, yi, py;
+	int numPasses = CONSUMER_HEIGHT/STRIPE_SIZE;
+	float producer_buffer[PRODUCER_WIDTH][STRIPE_SIZE + 1];
+	float ** consumer_arr;
+	float ** returnMePls;
+	int correctness = 0;
+
+	if( CONSUMER_HEIGHT % STRIPE_SIZE )
+		numPasses ++; 
+
+	printf("Good morning\n");
+	consumer_arr = malloc(CONSUMER_WIDTH*sizeof(float*));
+        for( i = 0; i < CONSUMER_WIDTH; i ++ )
+                consumer_arr[i] = calloc(CONSUMER_HEIGHT, sizeof(float));
+
+	
+	/*This loop executes the producer and consumer function in parallel strips
+		of 16*/
+	#pragma omp parallel for private(yo, y_in, y_base, x, yi, py, producer_buffer)
+	for( yo = 0; yo < numPasses; yo++ )
+	{
+		y_base = STRIPE_SIZE * yo;
+		if (y_base > CONSUMER_HEIGHT - STRIPE_SIZE)
+			y_base = CONSUMER_HEIGHT - STRIPE_SIZE;
+		/*Precompute the entire section of the producer we need*/
+		for( y_in = 0; y_in <= STRIPE_SIZE; y_in ++ )
+		{
+			/*This goes up to STRIPE_SIZE + 1 because the stripe size describes how
+				big the consumer function's stripes are, which are one smaller than the
+				producer function */
+			y = y_base + y_in;
+			for( x = 0; x < PRODUCER_WIDTH; x ++)
+			{
+				producer_buffer[x][y_in] = producer(x,y); //Now say that five times fast
+			}
+		}
+		/*Now compute this section of the consumer*/
+		for( x = 0; x < CONSUMER_WIDTH; x ++ )
+		{
+			for( y_in = 0; y_in < STRIPE_SIZE; y_in ++ )
+			{
+				y = y_base + y_in;
+				consumer_arr[x][y] = consumer_from_buffer(x, y, (float**)producer_buffer);
+			}
+		}
+	}
+	return returnMePls;
+//	return consumer_arr;
+}
