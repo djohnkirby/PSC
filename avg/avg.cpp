f37e2e0f9ccab8afabf0e0ad2d4c3b344e6ac66a
djohnkirby <dkirby@cmu.edu>
2014-02-27 21:49:31 -0500

    expected constructor....

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -1,6 +1,12 @@
 #include <Halide.h>
 #include <stdio.h>
-
+#include "../apps/support/image_io.h"
 
 using namespace Halide;
 
+int main( int argc, char ** argv )
+{
+  Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
+	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
+	
+}
