8b187aa6e51a5f5e4f3b64335404453203682d3b
Daniel John Kirby <dkirby@cmu.edu>
2014-03-01 19:18:04 -0500

    Don't clobber.

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -9,14 +9,13 @@ using namespace Halide;
 int main( int argc, char ** argv )
 {
 	Halide::Func average;
-	Halide::Var x, y, c;
+	Halide::Var x, y, c1, c2;
         Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
 	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
 	
-	average(x, y, c) = Halide::cast<uint8_t>((input1(x,y,c)*1.0f + input2(x,y,c)*1.0f)*0.5f);
+	average(x, y, c) = Halide::cast<uint8_t>((input1(x,y,c1)*1.0f + input2(x,y,c2)*1.0f)*0.5f);
 
-	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), \
-																									min(input1.height(), input2.height()), 
+	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), 
 	
 	return 0;
 }
