f2072294f118d93fd3aaf1b7b5e0d2b5f1710eb9
Daniel John Kirby <dkirby@cmu.edu>
2014-03-01 19:50:10 -0500

    Wonky compilation errors, maybe take this one to Andrew? But first take some time to look at the examples in /apps/

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -1,3 +1,8 @@
+/* This is a program that simply averages two images.
+ * The output image is guarunteed to be the size of the smaller (in both dimensions)
+ * of the two images. So for instance if the inputs are (800 x 600) and (600 x 800)
+ * the output image will be (600 x 600)*/
+
 #include <Halide.h>
 #include <stdio.h>
 
@@ -13,9 +18,9 @@ int main( int argc, char ** argv )
         Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
 	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
 	
-	average(x, y, c) = Halide::cast<uint8_t>((input1(x,y,c1)*1.0f + input2(x,y,c2)*1.0f)*0.5f);
+	average(x, y, c1, c2) = Halide::cast<uint8_t>((input1(x,y,c1)*1.0f + input2(x,y,c2)*1.0f)*0.5f);
 
-	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), 
+	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), input1.channels(), input2.channels()); 
 	
 	return 0;
 }
