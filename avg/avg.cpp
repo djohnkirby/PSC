46c77d444b00873acc113f056270b8b38af50e3e
Daniel John Kirby <dkirby@cmu.edu>
2014-03-01 19:00:04 -0500

    Fixed lack of vars x and y in average

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -9,10 +9,11 @@ using namespace Halide;
 int main( int argc, char ** argv )
 {
 	Halide::Func average;
-  Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
+	Halide::Var x, y, c;
+        Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
 	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
 	
-	average(x, y, c) = Halide::cast<uint8_t>((input1(x,y,c)*1.0f + input2(x,y,c)*1.0f)*0.5f):
+	average(x, y, c) = Halide::cast<uint8_t>((input1(x,y,c)*1.0f + input2(x,y,c)*1.0f)*0.5f);
 
 	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), \
 																									min(input1.height(), input2.height()), 
