b7c72914528f4a5571c169a815980b026fb26279
djohnkirby <dkirby@cmu.edu>
2014-03-03 15:52:06 -0500

    Compiles now, not sure if the output is correct. will write a tester.

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -15,12 +15,14 @@ int main( int argc, char ** argv )
 {
 	Halide::Func average;
 	Halide::Var x, y, c1, c2;
-        Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
+  Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
 	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
 	
 	average(x, y, c1, c2) = Halide::cast<uint8_t>((input1(x,y,c1)*1.0f + input2(x,y,c2)*1.0f)*0.5f);
 
-	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), input1.channels(), input2.channels()); 
-	
+//	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), input1.channels(), input2.channels()); 
+//	save(output, "result.png");
+
+	Halide::Image<uint8_t> output = average.realize(input1.width(), input2.height(), input1.channels(), input2.channels());	
 	return 0;
 }
