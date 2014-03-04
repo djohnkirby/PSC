f6c8cd3b14bb5f8621fc06aa2f71f35b279ddb74
Daniel John Kirby <dkirby@cmu.edu>
2014-03-03 21:00:06 -0500

    no dice.

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -14,12 +14,13 @@ using namespace Halide;
 int main( int argc, char ** argv )
 {
 	Halide::Func average;
-	Halide::Var x, y, c1, c2;
-  Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
+	Halide::Var x("x"), y("y"), c("c"),c1("c1"), c2("c2");
+        Halide::Image<uint8_t> input1 = load<uint8_t>("input1.png");
 	Halide::Image<uint8_t> input2 = load<uint8_t>("input2.png");
 	
+//	Halide::Expr av = (input1(x, y, c1) + input2(x, y, c2))/2;
+//	average = av;
 	average(x, y, c1, c2) = Halide::cast<uint8_t>((Halide::cast<uint32_t>(input1(x,y,c1))*1.0f + Halide::cast<uint32_t>(input2(x,y,c2))*1.0f)*0.5f);
-
 //	Halide::Image<uint8_t> output = average.realize(min(input1.width(),input2.width()), min(input1.height(), input2.height()), input1.channels(), input2.channels()); 
 //	save(output, "result.png");
 	/*int w1 = input1.width();
@@ -32,7 +33,7 @@ int main( int argc, char ** argv )
 
 	Halide::Image<uint8_t> output = average.realize(input1.width(), input2.height(), input1.channels(), input2.channels());
 
-	for( int j = 0; j < output.height(); j ++ )
+/*	for( int j = 0; j < output.height(); j ++ )
 		for ( int i = 0; i < output.width(); i ++ )
 			if ( output(i,j) != (input1(i,j) + input2(i,j))/2 )
 				{
@@ -41,6 +42,6 @@ int main( int argc, char ** argv )
                        i, j, (input1(i,j)+input2(i,j))/2, output(i, j));
 				}
 
-	
+*/	
 	return 0;
 }
