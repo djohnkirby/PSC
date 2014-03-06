22aac98def3c18cc39d549f0ba8cf0327e31e108
djohnkirby <dkirby@cmu.edu>
2014-03-06 17:12:53 -0500

    Weird...doesn't seem to see load_tiff.

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -10,14 +10,97 @@
 
 // The only Halide header file you need is Halide.h. It includes all of Halide.
 #include <Halide.h>
-#include <halide_load_tiff.h>
+//#include <halide_load_tiff.h>
 // Include some support code for loading pngs. It assumes there's an
 // Image type, so we'll pull the one from Halide namespace;
+using namespace Halide;
 using Halide::Image;
 #include "../apps/support/image_io.h"
 #include <tiffio.h>
-#include "halide_load_tiff.h"
+//#include "halide_load_tiff.h"
 int debug = 0;
+
+template <typename U,typename T>
+void scan_line(TIFF* tif, Image<T> im)
+{
+        uint32 imagelength;
+        TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &imagelength);
+        tsize_t scanline = TIFFScanlineSize(tif);
+        T *ptr = (T*)im.data();
+        U* buf = (U*)_TIFFmalloc(scanline);
+        for (uint32 row = 0; row < imagelength; row++) {
+                TIFFReadScanline(tif, buf, row);
+                for (uint32 col = 0; col < scanline / sizeof(U); col++){
+                        convert(buf[col], *(ptr++));
+                }
+        }
+        _TIFFfree(buf);
+}
+
+template <typename T>
+Image<T> load_tiff(const char* filename) {
+        Func gray,gray2;
+        Var x,y,c;
+
+        TIFF* tif = TIFFOpen(filename,"r");
+        if (tif) {
+                uint32 w, h, ch, bits;
+                printf("Whoo! Entered the if statement\n");
+
+                TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &w);
+                TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &h);
+                /* THis is a hack fix, but a fix nonetheless, this can now
+                 * only do grayscale, maybe one day let's like not do this*/
+                //TIFFGetField(tif, TIFFTAG_SAMPLESPERPIXEL, &ch);
+                ch = 1;
+                TIFFGetField(tif, TIFFTAG_BITSPERSAMPLE, &bits);
+                printf("%d x %d x %d, %d bits\n", w, h, ch, bits);
+                Image<T> im(w,h,ch);
+                printf("%d x %d x %d, %d bits\n",w,h,ch,bits);
+
+                size_t npixels = w * h;
+
+
+                T *ptr = (T*)im.data();
+                int c_stride = im.stride(2);
+
+                _assert((ch == 3 && bits == 8) || (ch == 1 && (bits == 8 || bits == 16)),
+                                "Not supported format.\n");
+
+                if(ch == 3 && bits == 8){
+                        uint32* raster = (uint32*) _TIFFmalloc(npixels * sizeof (uint32));
+                        if (raster != NULL && TIFFReadRGBAImage(tif, w, h, raster, 0)) {
+                                for (int y = h - 1; y >= 0; y--) {
+                                        uint8_t* srcPtr = (uint8_t*)&(raster[w * y]);
+                                        for (int x = 0; x < w; x++) {
+                                                for (int c = 0; c < ch; c++) {
+                                                        convert(*srcPtr++, ptr[c*c_stride]);
+                                                }
+                                                srcPtr++;
+                                                ptr++;
+                                        }
+                                }
+                                _TIFFfree(raster);
+                        }
+                } else if (ch == 1){
+                        if (bits == 8) {
+                                scan_line<uint8_t,T>(tif,im);
+                        }else if (bits == 16) {
+                                scan_line<uint16_t,T>(tif,im);
+                        }
+                }else{
+                        _assert(false, "Not supported format.\n");
+                        return Image<T>(0,0,1);
+                }
+                TIFFClose(tif);
+
+                im.set_host_dirty();
+                return im;
+
+        }else{
+                return Image<T>(0,0,1);
+        }
+}
 
 
 int main(int argc, char **argv) 
@@ -33,7 +116,7 @@ int main(int argc, char **argv)
 		/*Halide::Image<uint8_t> input = load<uint8_t>(argv[1]);
 		Halide::Image<uint8_t> input2 = load<uint8_t>(argv[2]);
 		*/
-		Halide::Image<uint8_t> input = load_tiff(argv[0]);
+	  Halide::Image<uint8_t> input = load_tiff(argv[0]);
 		Halide::Image<uint8_t> input2 = load_tiff(argv[1]);
     Halide::Func average;
     Halide::Var x, y, c;
