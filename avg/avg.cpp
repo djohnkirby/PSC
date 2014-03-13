71be5403dc42bbf115f5baf3309aeb87b3eb272d
Daniel John Kirby <dkirby@cmu.edu>
2014-03-13 13:52:55 -0400

    compilation issues on laptop, running on cmist.

--- avg/avg.cpp
+++ avg/avg.cpp
@@ -18,13 +18,88 @@ static  __inline__ ticks getticks(void) {       // read the CPU cycle counter
         return ((ticks)a) | (((ticks)d) << 32);
 }
 
+void abort_(const char * s, ...)
+{
+        va_list args;
+        va_start(args, s);
+        vfprintf(stderr, s, args);
+        fprintf(stderr, "\n");
+        va_end(args);
+        abort();
+}
+
 struct image * read_png(char * file_name)
 {
-	struct image returnMe;
+	int x, y, width, height;
+	png_byte color_type;
+	png_byte bit_depth;
+
+	png_structp png_ptr;
+	png_infop info_ptr;
+	int number_of_passes;
+	png_bytep * row_pointers;
+
+	struct image * returnMe;
+	char header[8];
 	FILE *fp = fopen(file_name, "rb");
 	if(!fp)
 		exit(EXIT_FAILURE);
+	fread(header, 1, 8, fp);
+        if (png_sig_cmp(header, 0, 8))
+                abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+
+	/* initialize stuff */
+        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr)
+                abort_("[read_png_file] png_create_read_struct failed");
+
+        info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr)
+                abort_("[read_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[read_png_file] Error during init_io");
+
+        png_init_io(png_ptr, fp);
+        png_set_sig_bytes(png_ptr, 8);
+
+        png_read_info(png_ptr, info_ptr);
+
+        width = png_get_image_width(png_ptr, info_ptr);
+        height = png_get_image_height(png_ptr, info_ptr);
+        color_type = png_get_color_type(png_ptr, info_ptr);
+        bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+        number_of_passes = png_set_interlace_handling(png_ptr);
+        png_read_update_info(png_ptr, info_ptr);
 	
+	/* read file */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[read_png_file] Error during read_image");
+
+        row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+        for (y=0; y<height; y++)
+                row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+
+        png_read_image(png_ptr, row_pointers);
+	fclose(fp);
+
+	 for (y=0; y<height; y++) {
+                png_byte* row = row_pointers[y];
+                for (x=0; x<width; x++) {
+                        png_byte* ptr = &(row[x*4]);
+                        printf("Pixel at position [ %d - %d ] has RGBA values: %d - %d - %d - %d\n",
+                               x, y, ptr[0], ptr[1], ptr[2], ptr[3]);
+
+                        /* set red value to 0 and green value to the blue one */
+                        ptr[0] = 0;
+                        ptr[1] = ptr[2];
+                }
+        }
+
+
+	/* Write stuff here*/	
 	return(returnMe);
 }
 
