474398e86daf2b28db607832f64950a52110beb3
djohnkirby <dkirby@cmu.edu>
2014-03-16 10:44:10 -0400

    Trying without channels

--- avg/avg_h.cpp
+++ avg/avg_h.cpp
@@ -6,15 +6,15 @@ int main( int argc, char ** argv )
 {
 	ImageParam input(UInt(8), 3);
 	ImageParam input2(UInt(8), 3);
-	Halide::Func average;
+	Halide::Func average("average");
 
   Halide::Var x, y, xi, yi, c;
 
-  Halide::Expr value = input(x, y, c);
-  Halide::Expr value2 = input2(x, y, c);
+  Halide::Expr value = input(x, y);
+  Halide::Expr value2 = input2(x, y);
   value = value/2.0f + value2/2.0f;
 
-  average(x, y, c) = Halide::cast<uint8_t>(value);
+  average(x, y) = Halide::cast<uint8_t>(value);
 	
 	average.split(y,y,yi,8).parallel(y).vectorize(x,8);
 	
